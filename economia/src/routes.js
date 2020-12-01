cb5db2a754aa3d9357f7f81c0e3ff09d8e45d5c7
giovannabbottino <giovannabbottino@gmail.com>
2020-12-01 10:54:16 -0300

    remodela pastas e corrige bug

---
+++ economia/src/routes.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+import history from './history';
+
+import { isAuthenticated } from "./services/auth";
+import TelaLogin from "./containers/login";
+import TelaCadastro from "./containers/cadastro";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+          <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+        )
+    }
+  />
+);
+
+const Routes = () => (
+  <Router history={history}>
+    <Switch>
+      <Route path="/loginIn" component={TelaLogin} />
+      <Route path="/signIn" component={TelaCadastro} />
+      <PrivateRoute path="/app" component={() => <h1>App</h1>} />
+    </Switch>
+  </Router>
+);
+
+export default Routes;
