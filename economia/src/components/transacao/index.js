ac63ce3645487c8dab054a6e6cdc13a113e2dec0
giovannabbottino <giovannabbottino@gmail.com>
2020-12-02 12:28:36 -0300

    Adiciona tela de ediçao

--- economia/src/components/transacao/index.js
+++ economia/src/components/transacao/index.js
@@ -2,37 +2,55 @@ import React, { Component } from 'react';
 import "./styles.css";
 import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 import { Fade } from "react-reveal";
+import EditaTransacao from '../edita_transacao'
 
 export default class Transacao extends Component {
     constructor(props) {
         super(props);
-        this.state = this.props.transacaoDetails;
+        this.state = {
+            transacaoDetails: this.props.transacaoDetails,
+            showComponent: false,
+        }
+        this._onButtonClick = this._onButtonClick.bind(this);
+    }
+
+    _onButtonClick() {
+        this.setState({
+            showComponent: true,
+        });
     }
 
     render() {
         return (
-            <Fade top duration={1000} distance="20px">
-                <Card className='root' variant="outlined">
-                    <CardContent>
-                        <Typography className='title' color="textSecondary" gutterBottom>
-                            {this.state.nome}
-                        </Typography>
-                        <Typography variant="h5" component="h2">
-                            R${this.state.valor}
-                        </Typography>
-                        <Typography className='pos' color="textSecondary">
-                            {this.state.data}
-                        </Typography>
-                        <Typography variant="body2" component="p">
-                            {this.state.descricao}
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">Editar</Button>
-                        <Button size="small">Deletar</Button>
-                    </CardActions>
-                </Card>
-            </Fade>
+            <div>
+                <Fade top duration={1000} distance="20px">
+                    <Card className='root' variant="outlined">
+                        <CardContent>
+                            <Typography className='title' color="textSecondary" gutterBottom>
+                                {this.state.transacaoDetails.nome}
+                            </Typography>
+                            <Typography variant="h5" component="h2">
+                                R${this.state.transacaoDetails.valor}
+                            </Typography>
+                            <Typography className='pos' color="textSecondary">
+                                {this.state.transacaoDetails.data}
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                                {this.state.transacaoDetails.descricao}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small" onClick={this._onButtonClick}>Editar</Button>
+                            <Button size="small">Deletar</Button>
+                        </CardActions>
+                    </Card>
+                </Fade>
+                {
+                    this.state.showComponent ?
+                        <EditaTransacao transacaoDetails={this.state.transacaoDetails} /> :
+                        null
+                }
+            </div>
         );
     }
 }
--- economia/src/containers/app/index.js
+++ economia/src/containers/app/index.js
@@ -15,21 +15,25 @@ export default class App extends Component {
         };
         this.getTransacoes()
         this.transacaoDetails = [{
+            id: 1,
             nome: 'nome',
             valor: '500,00',
             descricao: 'descricao',
             data: '27/10/2020'
         }, {
+            id: 2,
             nome: 'nome',
             valor: '500,00',
             descricao: 'descricao',
             data: '20/10/2020'
         }, {
+            id: 3,
             nome: 'nome',
             valor: '500,00',
             descricao: 'descricao',
             data: '25/10/2020'
         }, {
+            id: 4,
             nome: 'nome',
             valor: '500,00',
             descricao: 'descricao',
@@ -74,6 +78,7 @@ export default class App extends Component {
                     return (
                         <Transacao
                             transacaoDetails={{
+                                id: experience.id,
                                 nome: experience.nome,
                                 descricao: experience.descricao,
                                 data: experience.data,
